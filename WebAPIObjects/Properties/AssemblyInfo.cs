5d4e011990c0b2f4051d12b3351871721841031d
Ben Bowen <bcbowen@gmail.com>
2013-05-16 23:26:20 -0400

    Added .nuspec

--- WebAPIObjects/Properties/AssemblyInfo.cs
+++ WebAPIObjects/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebAPIObjects")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Object library to be used with the TradeStation WebAPI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TradeStation")]
+[assembly: AssemblyCompany("Ben Bowen")]
 [assembly: AssemblyProduct("WebAPIObjects")]
-[assembly: AssemblyCopyright("Copyright © TradeStation 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Ben Bowen 2013")]
+[assembly: AssemblyTrademark("2013")]
 [assembly: AssemblyCulture("")]
 //[assembly: InternalsVisibleTo("WebAPIObjects.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100779791b03e145256d584d389aa7154024a9590955cf22c6c1ab2d019aff04d04254b71cc2e458e9bd353e0321bae330e21452ee401a590206bcb1070b35726eac226cc3e065d567dab047c405bbc3472f45890afc86c63fa57bd68b85548e39a07bae94d4e860fc3f9074e5a1c7f751ca868bbbcc57e8e30e73ca439692501bf")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
